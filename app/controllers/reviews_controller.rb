c7bece9719fec7b4562c1f88fd791984a8405079
frantz crozilhac <fran.stacksta@gmail.com>
2019-05-23 17:34:48 +0200

    reviews controller and views

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,26 @@
 class ReviewsController < ApplicationController
+  before_action :set_booking
   def new
+    @review = Review.new
   end
 
   def create
+    @review = Review.new(review_params)
+    @review.booking = @booking
+    if @review.save
+      redirect_to islands_path(@booking.island_id)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:booking_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :stars)
   end
 end
