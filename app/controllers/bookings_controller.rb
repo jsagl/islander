fd6d8b444dcfa880fb6f7b21a1c9f343ccd48807
Jean Saglio <jean.saglio@gmail.com>
2019-05-23 17:21:04 +0200

    removed 2 useless routes and coded bookings controller & view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,40 @@
 class BookingsController < ApplicationController
+  before_action :find_island, only: [:new, :create, :destroy]
+  before_action :find_booking, only: [:show, :destroy]
+
   def show
   end
 
   def new
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @booking.island = @island
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render "new"
+    end
   end
 
   def destroy
+    @booking.destroy
+    redirect_to island_path(@island)
+  end
+
+  private
+
+  def find_island
+    @island = Island.find(params[:island_id])
+  end
+
+  def find_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
   end
 end
