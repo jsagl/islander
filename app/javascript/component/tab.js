b1416c2da902f9dbeb39798f01b5bcc63d750fd2
frantz crozilhac <fran.stacksta@gmail.com>
2019-05-28 18:53:47 +0200

    dashboardupdate

--- app/javascript/component/tab.js
+++ app/javascript/component/tab.js
@@ -1,20 +1,30 @@
+const islandsGlobal = document.querySelector('#global-view')
 const myIslands = document.querySelector('#chart-view')
 const myBookings = document.querySelector('#table-view')
+const globIslandTabBtn = document.querySelector('#global-tab-btn')
 const islandTabBtn = document.querySelector('#island-tab-btn')
 const bookingTabBtn = document.querySelector('#booking-tab-btn')
 
 const tabManager = () => {
-  if (islandTabBtn) {
-    islandTabBtn.addEventListener('click', e => {
-      myIslands.classList.add('active')
-      myBookings.classList.remove('active')
-    })
+  // if (globIslandTabBtn) {
+  //   globIslandTabBtn.addEventListener('click', e => {
+  //     islandsGlobal.classList.add('active')
+  //     myIslands.classList.remove('active')
+  //     myBookings.classList.remove('active')
+  //   })
 
-    bookingTabBtn.addEventListener('click', e => {
-      myBookings.classList.add('active')
-      myIslands.classList.remove('active')
-    })
-  }
+  //   islandTabBtn.addEventListener('click', e => {
+  //     islandsGlobal.classList.remove('active')
+  //     myIslands.classList.add('active')
+  //     myBookings.classList.remove('active')
+  //   })
+
+  //   bookingTabBtn.addEventListener('click', e => {
+  //     islandsGlobal.classList.remove('active')
+  //     myIslands.classList.remove('active')
+  //     myBookings.classList.add('active')
+  //   })
+  // }
 }
 
 export { tabManager }
