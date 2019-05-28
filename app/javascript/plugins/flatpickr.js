9eb0b94863934db46a15a9151257b274ddf4fbb8
Jean Saglio <jean.saglio@gmail.com>
2019-05-28 16:22:39 +0200

    ok

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,7 +1,13 @@
 const flatpickr = require("flatpickr");
 
-flatpickr(".date-picker", {
-  minDate: "today"
+flatpickr(".start-date", {
+  minDate: "today",
+  defaultDate: "today"
+})
+
+flatpickr(".end-date", {
+  minDate: new Date().fp_incr(1),
+  defaultDate: new Date().fp_incr(1),
 })
 
 export { flatpickr };
